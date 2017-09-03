cbc8986b5052ac83e9583f71dccc220dcacca662
Michael Goldenberg <mgoldenberg6@gmail.com>
2017-09-03 17:57:12 +0000

    parameterize port number.

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 const express = require('express')
 const app = express()
 
+/* parse port number */
+let args = process.argv.slice(2);
+let port;
+
+if(args.length < 1) {
+  	console.log('must supply a port number.');
+	return;
+} else {
+	port = parseInt(args[0]);
+	if(port === NaN) {
+		console.log('invalid port specified: ' + args[0]);
+		return;
+	}
+}
+
+/* setup server */ 
 app.get('/', function (req, res) {
-  res.send('Goodbye World!')
+	res.send('Goodbye World!');
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(port, 'localhost', function () {
+	console.log('listening on: localhost:' + port);
 })
